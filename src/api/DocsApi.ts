38f229809ffa6366e56020d7af4079ecd3b90c2f
Олег Завьялов <O.romanowi4@gmail.com>
2022-07-09 02:56:59 +0700

    Метод апи для получения доков

--- src/api/DocsApi.ts
+++ src/api/DocsApi.ts
@@ -11,6 +11,30 @@ export class DocsApi {
         })
     }
 
+    getDocsTree = () => {
+        return fetch("/docs/domains/tree", {
+            method: 'GET'
+        }).then(response => {
+            if (response.status == 401)
+                return Promise.reject("unauthorized").then(v => v)
+            if (response.status == 403)
+                return Promise.reject("forbidden").then(v => v)
+            return response.json().then(value => value as Map<String, Array<String>>);
+        })
+    }
+
+    deleteDoc = (domain: String, filename: String) => {
+        return fetch("/docs/" + domain + "/" + filename, {
+            method: 'DELETE'
+        }).then(response => {
+            if (response.status == 401)
+                return Promise.reject("unauthorized").then(v => v)
+            if (response.status == 403)
+                return Promise.reject("forbidden").then(v => v)
+            return response.text().then(value => value);
+        })
+    }
+
     uploadFile = (domain: String, filename: String, body: FormData) => {
         return fetch("/docs/" + domain + "/" + filename, {
             method: 'POST',
